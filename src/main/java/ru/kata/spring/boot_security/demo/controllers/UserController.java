bdf23d8b0baa9e92d553f58a26ce30fc2917f13c
laverabl <podoprygina@MacBook-Air-Valeria.local>
2024-01-05 23:53:20 +0300

    del logger

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +15,6 @@ import java.security.Principal;
 public class UserController {
 
     private final UserService userService;
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     public UserController(UserService userService) {
@@ -27,7 +24,6 @@ public class UserController {
     @GetMapping("")
     public String showUser(Model model, Principal principal) {
         String username = principal.getName();
-        log.info("Username: {}", username);
         User user = userService.getUserByName(username);
         model.addAttribute("user", user);
         return "user-show";
